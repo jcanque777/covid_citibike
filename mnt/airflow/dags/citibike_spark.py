749b53f05c619a781e4df6d11f74e628a521a199
John Canque <jcanque@gmail.com>
2021-01-01 13:06:37 -0500

    feat: Added Spark Commands From Jupyter Notebook To Transform Data

--- mnt/airflow/dags/citibike_spark.py
+++ mnt/airflow/dags/citibike_spark.py
@@ -2,6 +2,7 @@ import os
 from pyspark.sql import SparkSession, Row
 from pyspark.sql import functions as F
 from pyspark.sql.functions import udf, col, lit, to_timestamp, year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.functions import hour, count, dayofyear
 from pyspark import SparkFiles
 import pandas as pd
 import requests
@@ -26,6 +27,35 @@ spark.conf.set("mapreduce.fileoutputcommitter.algorithm.version", "2")
 
 # read csv and save to df
 
-df = spark.read.csv(df = spark.read.csv("citidata/202011-citibike-tripdata.csv",header=True))
-df.printSchema()
+df = spark.read.csv("/citidata/202011-citibike-tripdata.csv",header=True)
 
+# turn stoptime column to timestamp from string
+df = df.withColumn("stoptime_ts",to_timestamp(col("stoptime")))
+
+# turn stoptime column to timestamp from string
+df = df.withColumn("starttime_ts",to_timestamp(col("starttime")))
+
+# select columns to drop
+columns_to_drop = ['tripduration',
+ 'starttime',
+ 'stoptime',
+ 'start station id',
+ 'start station latitude',
+ 'start station longitude',
+ 'end station id',
+ 'end station latitude',
+ 'end station longitude']
+
+df = df.drop(*columns_to_drop)
+
+df = df.withColumn("stoptime_date",date_format('stoptime_ts','MM/dd/yyy'))
+df = df.withColumn('stoptime_time', date_format('stoptime_ts', 'HH:mm:ss'))
+
+
+# create new dataframe grouping by time, location, and counting the number of bikes
+final_df = (df.groupBy("stoptime_date", hour("stoptime_ts").alias("hour"), "start station name", "end station name").agg(count("bikeid").alias("no_of_trips")))
+
+## Rename columns
+final_df = final_df.withColumnRenamed("start station name", "start_station_name").withColumnRenamed("end station name", "end_station_name")
+
+final_df.write.csv('transformed_citibike.csv')
